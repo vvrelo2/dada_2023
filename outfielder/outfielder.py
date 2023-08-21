1bf359298f84485a26fc0db652eb065cc02a212f
Hiram E. Perez <hiram.eps@gmail.com>
2023-08-21 09:22:10 -0500

    outfielder problem

---
+++ outfielder/outfielder.py
@@ -0,0 +1,29 @@
+'''
+outfielder.py
+
+C int ball_trajectory(int x) {}
+
+v1 = 0.99m/s
+v2 = 9.99m/s
+
+10x - 5x^2
+
+pip install matploit
+
+'''
+# %%
+import matplotlib.pyplot as plt
+
+def ball_trajectory(x):
+    location = 10 * x - 5 * (x**2)
+    return location
+
+xs = [x/100 for x in list(range(201))]
+ys = [ball_trajectory(x) for x in xs]
+plt.plot(xs, ys)
+plt.title('The trajectory of a thrown ball')
+plt.xlabel('Horizontal')
+plt.ylabel('Vertical')
+plt.show()
+
+# %%
