6aeab04fdb52af85d30e6f9ee16bd622d79b3512
Emilio Gudiño Varela <98443343+vvrelo2@users.noreply.github.com>
2023-09-19 21:43:49 -0600

    Add files via upload

---
+++ FibonacciGregory/GregoryLeibniz.js
@@ -0,0 +1,15 @@
+function generarFibonacci(N) {
+    var fibonacci = [0, 1]; // Inicializamos la serie con los dos primeros términos
+
+    for (var i = 2; i < N; i++) {
+        var siguienteTermino = fibonacci[i - 1] + fibonacci[i - 2];
+        fibonacci.push(siguienteTermino);
+    }
+
+    return fibonacci;
+}
+
+var N = 10; // Se cambia este valor para obtener más o menos términos
+var resultado = generarFibonacci(N);
+
+console.log("Los primeros " + N + " términos de la serie de Fibonacci son: " + resultado);
